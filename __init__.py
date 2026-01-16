6f24808b46149ef473603ebd3e1b5548a7bac516
Pauan <pauanyu+github@pm.me>
2026-01-15 21:29:21 -0800

    Version 1.0.0

--- __init__.py
+++ __init__.py
@@ -4,15 +4,6 @@ import os
 import nodes
 from comfy_config import config_parser
 
-__author__ = """Pauan"""
-__email__ = "pauanyu+github@pm.me"
-__version__ = "0.0.1"
-
-__all__ = [
-    "NODE_CLASS_MAPPINGS",
-    "NODE_DISPLAY_NAME_MAPPINGS",
-]
-
 custom_node_dir = os.path.dirname(os.path.realpath(__file__))
 
 project_config = config_parser.extract_node_configuration(custom_node_dir)
@@ -20,3 +11,12 @@ project_config = config_parser.extract_node_configuration(custom_node_dir)
 js_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "js")
 
 nodes.EXTENSION_WEB_DIRS[project_config.project.name] = js_dir
+
+__author__ = """Pauan"""
+__email__ = "pauanyu+github@pm.me"
+__version__ = project_config.project.version
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+]
